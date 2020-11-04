ec3d2f9c3ff0cc4c52d0ca4acd451206bb78ab8b
selimtraore <selimtraore@live.com>
2020-11-04 23:36:18 +0100

    index commit

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-mod connector;
-mod Departement;
+
 
 use rocket_contrib::templates::Template;
 use std::collections::HashMap;
+use rocket_contrib::serve::StaticFiles;
 
 #[macro_use] extern crate rocket;
 
 #[get("/")]
 fn index() -> Template {
    let mut context:HashMap<u32,u32> = HashMap::new();
-    connector::Connector::new("","");
-    Template::render("index",&context)    
+    Template::render("index",&context)
 
 }
 
@@ -20,6 +19,7 @@ fn main() {
 
 
     rocket::ignite()
+        .mount("/static",StaticFiles::from("./static"))
         .attach(Template::fairing())
         .mount("/", routes![index]).launch();
 }
